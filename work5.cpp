a6db3f377b9c1cda8ee6ff521498da552a1c596a
Sanal Boldyrev <55288025+MrClam1@users.noreply.github.com>
2019-10-16 09:41:22 +0300

    Program 5

--- work5.cpp
+++ work5.cpp
@@ -24,3 +24,40 @@ int main() {
 	}
 	return 0;
 }
+
+/*
+int main() {
+	const int N = 3, M = 3;
+	int mas[N][M];
+	int sum = 0;
+	int i, j;
+	cout << "Enter numbers in massive: \n";
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cin >> mas[i][j];
+		}
+	}
+	cout << "Your massive: \n";
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
+			cout << mas[i][j] << " ";
+		}
+		cout << endl;
+	}
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
+			sum = sum + mas[i][j];
+		}
+		cout << "Amount of numbers in " << i + 1 << " line: " << sum << endl;
+		sum = 0;
+	}
+	for (j = 0; j < N; j++) {
+		for (i = 0; i < M; i++) {
+			sum = sum + mas[i][j];
+		}
+		cout << "Amount of numbers in " << j + 1 << " column: " << sum << endl;
+		sum = 0;
+	}
+	return 0;
+}
+*/
