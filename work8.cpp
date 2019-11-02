6cc6ec48396c887ab858371caf3e2d3f2f969588
Sanal Boldyrev <55288025+MrClam1@users.noreply.github.com>
2019-11-02 19:38:24 +0300

    struct

--- work8.cpp
+++ work8.cpp
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 #include <Windows.h>
 #include <iostream>
 #include <string>
@@ -7,45 +7,46 @@
 using namespace std;
 
 struct Human {
-  char* name;
-  int weight;
-  int height;
+	char* name;
+	int weight;
+	int height;
 };
 
-int main () {
-  int max = 0;
-  int counts;
-  cout << "Enter counts of people: \n";
-  cin >> counts;
-  Human *users = new Human[counts];
-  cout << "Enter information about people";
-  for (int i = 0; i < counts; i++) {
-    char *name = new char[50];
-    int WEIGHT;
-    int HEIGHT;
-    cin.ignore();
-    cin.get(name, 50);
-    cout << "Enter weight of " << i + 1 << " human: \n";
-    cin >> WEIGHT;
-    cout << "Enter height of " << i + 1 << " human: \n";
-    cin >> HEIGHT;
-    Human people = {
-      name,
-      WEIGHT,
-      HEIGHT
-    };
-    users[i] = people;
-  }
-  for (int i = 0; i < counts; i++) {
-    if (max < users[i].weight) {
-      max = i;
-    }
-  }
-  cout << "The highest weight is " << users[max].weight << endl;
-  cout << "Human< who have the highest weight is " << users[max].name << endl;
-  for (int i = 0; i < counts; i++) {
-    delete[] users[i].name;;
-  }
-  delete[] users;
-  return 0;
+int main() {
+	int max = 0;
+	int counts;
+	cout << "Enter counts of people: \n";
+	cin >> counts;
+	Human* users = new Human[counts];
+	cout << "Enter information about people! \n";
+	for (int i = 0; i < counts; i++) {
+		char* name = new char[50];
+		int WEIGHT;
+		int HEIGHT;
+		cin.ignore();
+		cout << "Enter name of " << i + 1 << " human: \n";
+		cin.get(name, 50);
+		cout << "Enter weight of " << i + 1 << " human: \n";
+		cin >> WEIGHT;
+		cout << "Enter height of " << i + 1 << " human: \n";
+		cin >> HEIGHT;
+		Human people = {
+		  name,
+		  WEIGHT,
+		  HEIGHT
+		};
+		users[i] = people;
+	}
+	for (int i = 0; i < counts; i++) {
+		if (max < users[i].weight) {
+			max = i;
+		}
+	}
+	cout << "The highest weight is " << users[max].weight << endl;
+	cout << "Human< who have the highest weight is " << users[max].name << endl;
+	for (int i = 0; i < counts; i++) {
+		delete[] users[i].name;;
+	}
+	delete[] users;
+	return 0;
 }
